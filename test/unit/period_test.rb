075628fd4f6b64c270c40c1fb4d401865c1e427b
Paul Damer <pdamer@gmail.com>
2006-05-20 03:50:40 +0000

    merging periods ...

---
+++ test/unit/period_test.rb
@@ -0,0 +1,10 @@
+require File.dirname(__FILE__) + '/../test_helper'
+
+class PeriodTest < Test::Unit::TestCase
+  fixtures :periods
+
+  # Replace this with your real tests.
+  def test_truth
+    assert true
+  end
+end
