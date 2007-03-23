b4a3f87eb0339bd0820888e82bd8e8e65d1651a2
Paul Damer <pdamer@gmail.com>
2007-03-23 17:32:14 +0000

    Merging initial alternatives

---
+++ app/helpers/alt_widget_helper.rb
@@ -0,0 +1,2 @@
+module AltWidgetHelper
+end
---
+++ test/unit/alt_widget_test.rb
@@ -0,0 +1,10 @@
+require File.dirname(__FILE__) + '/../test_helper'
+
+class AltWidgetTest < Test::Unit::TestCase
+  fixtures :alt_widgets
+
+  # Replace this with your real tests.
+  def test_truth
+    assert true
+  end
+end
