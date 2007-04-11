b6adba9c694cc1a9d2b1a12574217f00758e29df
Paul Damer <pdamer@gmail.com>
2007-04-11 09:50:53 +0000

    Updated alts, you have to reload the alt widget fixtures from the file

--- app/controllers/alt_widget_controller.rb
+++ app/controllers/alt_widget_controller.rb
@@ -1,17 +1,10 @@
 class AltWidgetController < ApplicationController
   #verify :xhr => true
   def view
-    #only a proof of concept
-    #this should probably be done by passing 
-    #some resource to the model and let the model 
-    #do so rjs or something
-    
     meeting = Meeting.find(params[:meeting_id])
-    s = AltWidget.find_random()[0].calculate_alternative(meeting)
+    alt = AltWidget.find_random()
     render :update do |page|
-      #instead pass page to calculate_alternative or another one 
-      #called render alternative or something
-      page.replace_html :alternative, s
+      alt.render(meeting, page, :alternative)
     end
   end
 end
