8733a1ece4418eac641bc0959246ea8ed6bdf866
pdamer <pdamer@0889de32-30e5-0310-ab80-ca79f5f4c722>
2007-10-23 02:36:34 +0000

    adding invites model
    git-svn-id: https://jsv.mine.nu/svn/repos/mc/trunk@214 0889de32-30e5-0310-ab80-ca79f5f4c722

--- db/schema.rb
+++ db/schema.rb
@@ -2,7 +2,7 @@
 # migrations feature of ActiveRecord to incrementally modify your database, and
 # then regenerate this schema definition.
 
-ActiveRecord::Schema.define(:version => 6) do
+ActiveRecord::Schema.define(:version => 8) do
 
   create_table "alt_widgets", :force => true do |t|
     t.column "name",         :string
@@ -11,6 +11,13 @@ ActiveRecord::Schema.define(:version => 6) do
     t.column "last_used_at", :datetime
     t.column "views",        :integer
     t.column "data",         :text
+  end
+
+  create_table "invites", :force => true do |t|
+    t.column "message",          :string
+    t.column "inviter_user_id",  :integer
+    t.column "accepter_user_id", :integer
+    t.column "token",            :string
   end
 
   create_table "meetings", :force => true do |t|
@@ -33,6 +40,17 @@ ActiveRecord::Schema.define(:version => 6) do
     t.column "created_at", :datetime
     t.column "stopped_at", :datetime
     t.column "descr",      :string
+  end
+
+  create_table "users", :force => true do |t|
+    t.column "login",                     :string
+    t.column "email",                     :string
+    t.column "crypted_password",          :string,   :limit => 40
+    t.column "salt",                      :string,   :limit => 40
+    t.column "created_at",                :datetime
+    t.column "updated_at",                :datetime
+    t.column "remember_token",            :string
+    t.column "remember_token_expires_at", :datetime
   end
 
 end
