6a17a943aacd9f73d29ec3d94a1206cbf6e5e7a7
pdamer <pdamer@0889de32-30e5-0310-ab80-ca79f5f4c722>
2007-11-08 14:36:30 +0000

    names
    git-svn-id: https://jsv.mine.nu/svn/repos/mc/trunk@223 0889de32-30e5-0310-ab80-ca79f5f4c722

--- app/models/invite.rb
+++ app/models/invite.rb
@@ -1,2 +1,21 @@
+# == Schema Information
+# Schema version: 8
+#
+# Table name: invites
+#
+#  id               :integer(11)   not null, primary key
+#  message          :string(255)   
+#  inviter_user_id  :integer(11)   
+#  accepter_user_id :integer(11)   
+#  token            :string(255)   
+#
+
 class Invite < ActiveRecord::Base
+  include TokenGenerator
+  before_create :set_token
+  
+  belongs_to :inviter_user, :class => 'User', :foreign_key => 'inviter_user_id'
+  belongs_to :accepter_user, :class => 'User', :foreign_key => 'accepter_user_id'
+  
+  
 end
--- app/models/meeting.rb
+++ app/models/meeting.rb
@@ -1,5 +1,5 @@
 # == Schema Information
-# Schema version: 6
+# Schema version: 8
 #
 # Table name: meetings
 #
