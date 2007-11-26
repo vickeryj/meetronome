0ecb7eda8672583ae64235e9bef9d32ff7947694
pdamer <pdamer@0889de32-30e5-0310-ab80-ca79f5f4c722>
2007-11-26 03:56:07 +0000

    fixes for upgraded rails
    git-svn-id: https://jsv.mine.nu/svn/repos/mc/trunk@247 0889de32-30e5-0310-ab80-ca79f5f4c722

--- config/environment.rb
+++ config/environment.rb
@@ -26,7 +26,8 @@ Rails::Initializer.run do |config|
   # Use the database for sessions instead of the file system
   # (create the session table with 'rake db:sessions:create')
   # config.action_controller.session_store = :active_record_store
-
+  config.action_controller.session = { :session_key => "_meetrenome_session", :secret => "Meetrenome is a cool name" }
+  
   # Use SQL instead of Active Record's schema dumper when creating the test database.
   # This is necessary if your schema can't be completely dumped by the schema dumper, 
   # like if you have constraints or database-specific column types
--- vendor/plugins/scriptaculous_slider/init.rb
+++ vendor/plugins/scriptaculous_slider/init.rb
@@ -1,2 +1,6 @@
+#changes from http://dev.rubyonrails.org/ticket/8633
+
+require "#{File.dirname(__FILE__)}/lib/helpers/slider_helper"
+
 ActionView::Helpers::AssetTagHelper::register_javascript_include_default "slider"
-ActionView::Base::load_helpers "#{directory}/lib/helpers/"
+ActionView::Base.send :include, ActionView::Helpers::SliderHelper
