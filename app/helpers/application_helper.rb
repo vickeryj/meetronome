a5b8d1bdb8a53f22b0c0adb4fdc71909ecac23d4
pdamer <pdamer@0889de32-30e5-0310-ab80-ca79f5f4c722>
2007-12-17 01:02:08 +0000

    more signup view changes, also added helpers to use the buttons
    git-svn-id: https://jsv.mine.nu/svn/repos/mc/trunk@284 0889de32-30e5-0310-ab80-ca79f5f4c722

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,10 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+  def meet_link_to(name, options = {}, html_options = {}, *parameters_for_method_reference)
+    link_to(content_tag(:span, name), options, {:onclick=>"this.blur;", :class => "button"}.merge(html_options))
+  end
+  
+  def meet_link_to_function(name, *args, &block)
+    link_to_function content_tag(:span, name), args, :onclick=>"this.blur;", :class=>"button"
+  end
 end
