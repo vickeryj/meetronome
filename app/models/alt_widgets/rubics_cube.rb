2e899c5a2d06be7b64cee4c4d54a91c2012108dd
vickeryj <vickeryj@0889de32-30e5-0310-ab80-ca79f5f4c722>
2008-04-01 16:57:38 +0000

    fix typos
    git-svn-id: https://jsv.mine.nu/svn/repos/mc/trunk@355 0889de32-30e5-0310-ab80-ca79f5f4c722

--- app/models/alt_widgets/rubics_cube.rb
+++ app/models/alt_widgets/rubics_cube.rb
@@ -1,5 +1,5 @@
 class RubicsCube < AltWidget
   def render(meeting, page, tag)
-    page.replace_html tag, "With practice you could have solved #{(meeting.time_in_seconds/25).to_int } rubik's cubes during this meeting"
+    page.replace_html tag, "With practice you could have solved #{(meeting.time_in_seconds/25).to_int } Rubik's cubes during this meeting"
   end
 end
